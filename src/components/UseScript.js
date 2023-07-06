9975caf28a6c03cc917583d2d6969a33ae15dd34
vllllktoria <vika.v1ktoria@mail.ru>
2023-07-06 22:45:53 +0500

    auth 2.0

---
+++ src/components/UseScript.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+
+const UseScript = url => {
+  useEffect(() => {
+
+    const script = document.createElement('script')
+    script.src = url
+    script.async = true
+    script.setAttribute('data-telegram-login', 'samplebot')
+    script.setAttribute('data-size', 'large')
+    script.setAttribute('data-onauth', 'onTelegramAuth(user)')
+    script.setAttribute('data-request-access', 'write')
+    script.type = "text/javascript"
+
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [url])
+  
+};
+
+export default UseScript
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,72 +1,27 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react'
+import UseScript from '../components/UseScript'
 
-function Login() {
+function Auth() {
 
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [confirmationCode, setConfirmationCode] = useState('');
-  const [isCodeSent, setIsCodeSent] = useState(false);
-  const botToken = '';
+  UseScript('https://telegram.org/js/telegram-widget.js?22')
 
-  const handlePhoneNumberChange = (e) => {
-    setPhoneNumber(e.target.value);
-  };
-
-  const handleConfirmationCodeChange = (e) => {
-    setConfirmationCode(e.target.value);
-  };
-
-  const handleSendCode = async () => {
-    try {
-      const response = await axios.post(
-        `https://api.telegram.org/bot${botToken}/auth.sendCode`,
-        {
-          phone_number: phoneNumber,
-        }
-      );
-      console.log(response.data);
-      setIsCodeSent(true);
-    } catch (error) {
-      console.error(error);
+  function onTelegramAuth(user){
+      alert(
+        'Logged in as ' +
+          user.first_name +
+          ' ' +
+          user.last_name +
+          ' (' +
+          user.id +
+          (user.username ? ', @' + user.username : '') +
+          ')'
+      )
     }
-  };
 
-  const handleSignIn = async () => {
-    try {
-      const response = await axios.post(
-        `https://api.telegram.org/bot${botToken}/auth.signIn`,
-        {
-          phone_number: phoneNumber,
-          phone_code: confirmationCode,
-        }
-      );
+    return () => {  
+    
+    onTelegramAuth()
+    };
+}
 
-      console.log(response.data);
-
-      const token = response.data.result.auth_token;
-      localStorage.setItem('auth_token', token);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <div id ="login">
-      <h2 id="loginTxt">Login</h2>
-      {!isCodeSent && (
-        <div>
-          <input id="phoneNum" placeholder="Номер телефона" type="text" value={phoneNumber} onChange={handlePhoneNumberChange} />
-          <button id="sendCode"onClick={handleSendCode}>Получить код</button>
-        </div>
-      )}
-      {isCodeSent && (
-        <div>
-          <input  placeholder="Код подтверждения" type="text" value={confirmationCode} onChange={handleConfirmationCodeChange} />
-          <button onClick={handleSignIn}>Войти</button>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Login;
+export default Auth
